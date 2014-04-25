c233b8d9376787e619b663934efe0548f4ce1778
le-ney <le_ney@yahoo.com>
2014-04-24 23:02:46 -0400

    comments added

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+#the makeCacheMatrix is a function which takes an argument x of type of matrix
+#It returns an creates a special matrix which is a list 
+#with 4 items where each item is a function wrapped 
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
-        set <- function(y){
-                x <<- y
-                i <<- NULL
+        if(nrow(x) != ncol(x)){ # if input matrix is not square return null
+                message("Erorr: the input must be a square matrix")
+                return(i)
         }
-        get <- function() x
+        set <- function(y){ 
+                x <<- y    # check parent environments for an existing definition
+                i <<- NULL 
+        }
+        get <- function() x 
         setinverse <- function(solve) i <<- solve
         getinverse <- function() i
         list(set = set, get = get,
@@ -17,8 +20,9 @@ makeCacheMatrix <- function(x = matrix()) {
              getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+#The cacheSolve function uses makeCacheMatrix function in its implementation. 
+#It takes an argument x of type of special matrix (output of the makeCacheMatrix function). 
+#The out of this function is the inverse matrix which either is retrieved from cache or computed.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
